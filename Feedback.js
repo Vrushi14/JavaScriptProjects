4fd1a4c6a8b080331e4894fab747c454debdd44d
Vrushi14 <vrushi1214@gmail.com>
2024-12-18 13:10:26 +0530

    new file

--- Feedback.js
+++ Feedback.js
@@ -1,20 +1,12 @@
-//FeedBack Form
-let name = document.getElementById("name1").value
-document.getElementById("FF").textContent = "Feedback Form"
+// function Input() {
 
-document.getElementById("name1").textContent = "Name:"
-
-document.getElementById("email1").textContent = "Email:"
-
-document.getElementById("div1").textContent = "Division:"
-document.getElementById("op1").textContent = "A"
-document.getElementById("op2").textContent = "B"
-document.getElementById("op3").textContent = "C"
-document.getElementById("op4").textContent = "D"
-
-document.getElementById("fd").textContent = "Feedback:"
-
-document.getElementById("sub").textContent = "Submit"
-function show() {
-    document.getElementById("myP").textContent = `Thankyou for your Feedback ${ name }`
-}
+//     let name = document.getElementById("name").value;
+//     let email = document.getElementById("email").value;
+//     let div = document.getElementById("div").value;
+//     let feedback = document.getElementById("fd").value;
+  
+    
+//     document.getElementById("myP").value = `Thank you ! Hello, my name is ${name}, my email is ${email}, my division is ${div} and your feedback bright my future: ${feedback} ` ;
+//     console.log(myP)
+    
+//   }
